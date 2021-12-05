7b797abc92a961f1896d9c6f510dc396a1cdec6a
Davit9494 <davitsahakyan94@mail.ru>
2021-12-06 00:00:50 +0400

    feat:created oddOrEven.js file and solved task about number is odd or even

---
+++ oddOrEven.js
@@ -0,0 +1,7 @@
+//Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+let num = 125;
+if (num % 2 === 0) {
+  console.log(`${num} is even`);
+} else {
+  console.log(`${num} is odd`);
+}
